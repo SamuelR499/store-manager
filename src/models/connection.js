693124aba128ce285b97e012e1ba0e737ae6eb5b
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-17 13:28:20 -0300

    refactor: em connection altera passowrd de hardcoded para Variavel de ambiente

--- src/models/connection.js
+++ src/models/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT || 3306,
   user: 'root',
-  password: 'password',
+  password: process.env.MYSQL_PASSWORD,
   database: 'StoreManager',
 });
 module.exports = connection;
