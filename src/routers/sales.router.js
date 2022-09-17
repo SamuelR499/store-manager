dd8a55a4549da372ef20f7398a514e209a94360f
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-17 13:28:41 -0300

    feat: adiciona rota para deletar uma venda

--- src/routers/sales.router.js
+++ src/routers/sales.router.js
@@ -6,5 +6,6 @@ const saleRouter = express.Router();
 saleRouter.post('/', salesController.addSales);
 saleRouter.get('/', salesController.getSales);
 saleRouter.get('/:id', salesController.getSalesById);
+saleRouter.delete('/:id', salesController.deleteSale);
 
 module.exports = saleRouter;
